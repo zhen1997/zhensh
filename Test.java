70ed46ca80539db2a1e7def9cf5abb2d34e33061
yufeng0312 <2577189862@qq.com>
2019-05-20 10:53:31 +0800

    陈思宇

--- Test.java
+++ Test.java
@@ -13,3 +13,19 @@ public class Test {
         return instance;
     }
 }
+
+/** ³ÂË¼Óî Ë«ÖØÅĞ¶¨Ëø */
+public class Test {
+    private volatile static Test instance = null;
+    private Test(){}
+    private static Test getInstance(){
+        if (null == instance){
+            synchronized (Test.class){
+                if(null == instance){
+                    instance = new Test();
+                }
+            }
+        }
+        return instance;
+    }
+}
