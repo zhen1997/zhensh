146ae2e766d8c0b80aa08c7b7f08f83b60f38f05
yufeng0312 <2577189862@qq.com>
2019-05-20 10:57:40 +0800

    111

--- Test.java
+++ Test.java
@@ -15,17 +15,3 @@ public class Test {
 }
 
 /** ³ÂË¼Óî Ë«ÖØÅĞ¶¨Ëø */
-public class Test {
-    private volatile static Test instance = null;
-    private Test(){}
-    private static Test getInstance(){
-        if (null == instance){
-            synchronized (Test.class){
-                if(null == instance){
-                    instance = new Test();
-                }
-            }
-        }
-        return instance;
-    }
-}
